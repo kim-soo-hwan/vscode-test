b7ceb64726cc6c381488af6aa04ac3264b6b3bfc
Soohwan Kim <kimsoohwan@sunmoon.ac.kr>
2020-06-24 23:26:09 +0900

    Introduce an infinite loop

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,9 +3,36 @@ from random import randint
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
-# get the user input
-player = input("Rock, Paper, Scissors? ")
+while True:
 
-# assign a random play to the computer
-computer = play[randint(0, 2)]
-print('Computer: {}'.format(computer))
+    # get the user input
+    player = input("Rock, Paper, Scissors? ")
+    
+    # assign a random play to the computer
+    computer = play[randint(0, 2)]
+    print('Computer: {}'.format(computer))
+    
+    # tie
+    if player == computer:
+        print("Tie!")  
+        
+    # rock
+    elif player == "Rock":
+        if computer == "Scissors":
+            print("You win!")
+        else:
+            print("You lose!")
+    
+    # paper
+    elif player == "Paper":
+        if computer == "Rock":
+            print("You win!")
+        else:
+            print("You lose!")
+    
+    # scissors
+    elif player == "Scissors":
+        if computer == "Paper":
+            print("You win!")
+        else:
+            print("You lose!")        
